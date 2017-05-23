782223e181992899bf0c3d6763563ee0b89a0d0f
skappa <denaltro@gmail.com>
2017-05-24 03:54:16 +0400

    fix

---
+++ Diplom/AddressesForm.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Diplom
+{
+    partial class AddressesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button_search = new System.Windows.Forms.Button();
+            this.button_add = new System.Windows.Forms.Button();
+            this.button_update = new System.Windows.Forms.Button();
+            this.button_remove = new System.Windows.Forms.Button();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Street = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.House = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Building = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Apartment = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.Street,
+            this.House,
+            this.Building,
+            this.Apartment});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 46);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(673, 446);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(581, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button_search
+            // 
+            this.button_search.Location = new System.Drawing.Point(610, 12);
+            this.button_search.Name = "button_search";
+            this.button_search.Size = new System.Drawing.Size(75, 23);
+            this.button_search.TabIndex = 2;
+            this.button_search.Text = "Найти";
+            this.button_search.UseVisualStyleBackColor = true;
+            this.button_search.Click += new System.EventHandler(this.button_search_Click);
+            // 
+            // button_add
+            // 
+            this.button_add.Location = new System.Drawing.Point(448, 498);
+            this.button_add.Name = "button_add";
+            this.button_add.Size = new System.Drawing.Size(75, 23);
+            this.button_add.TabIndex = 3;
+            this.button_add.Text = "Добавить";
+            this.button_add.UseVisualStyleBackColor = true;
+            this.button_add.Click += new System.EventHandler(this.button_add_Click);
+            // 
+            // button_update
+            // 
+            this.button_update.Location = new System.Drawing.Point(529, 498);
+            this.button_update.Name = "button_update";
+            this.button_update.Size = new System.Drawing.Size(75, 23);
+            this.button_update.TabIndex = 4;
+            this.button_update.Text = "Изменить";
+            this.button_update.UseVisualStyleBackColor = true;
+            this.button_update.Click += new System.EventHandler(this.button_update_Click);
+            // 
+            // button_remove
+            // 
+            this.button_remove.Location = new System.Drawing.Point(610, 498);
+            this.button_remove.Name = "button_remove";
+            this.button_remove.Size = new System.Drawing.Size(75, 23);
+            this.button_remove.TabIndex = 5;
+            this.button_remove.Text = "Удалить";
+            this.button_remove.UseVisualStyleBackColor = true;
+            this.button_remove.Click += new System.EventHandler(this.button_remove_Click);
+            // 
+            // Id
+            // 
+            this.Id.HeaderText = "Идентификатор";
+            this.Id.Name = "Id";
+            this.Id.Visible = false;
+            // 
+            // Street
+            // 
+            this.Street.FillWeight = 203.0457F;
+            this.Street.HeaderText = "Улица";
+            this.Street.Name = "Street";
+            // 
+            // House
+            // 
+            this.House.FillWeight = 65.65144F;
+            this.House.HeaderText = "Дом";
+            this.House.Name = "House";
+            // 
+            // Building
+            // 
+            this.Building.FillWeight = 65.65144F;
+            this.Building.HeaderText = "Строение";
+            this.Building.Name = "Building";
+            // 
+            // Apartment
+            // 
+            this.Apartment.FillWeight = 65.65144F;
+            this.Apartment.HeaderText = "Квартира";
+            this.Apartment.Name = "Apartment";
+            // 
+            // AddressesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 529);
+            this.Controls.Add(this.button_remove);
+            this.Controls.Add(this.button_update);
+            this.Controls.Add(this.button_add);
+            this.Controls.Add(this.button_search);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "AddressesForm";
+            this.Text = "AddressesForm";
+            this.Activated += new System.EventHandler(this.AddressesForm_Activated);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button_search;
+        private System.Windows.Forms.Button button_add;
+        private System.Windows.Forms.Button button_update;
+        private System.Windows.Forms.Button button_remove;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Street;
+        private System.Windows.Forms.DataGridViewTextBoxColumn House;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Building;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Apartment;
+    }
+}
---
+++ Diplom/OrgEventForm.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Diplom.Models;
+using Diplom.Repository;
+
+namespace Diplom
+{
+    public partial class OrgEventForm : Form
+    {
+        private OrgEvent OrgEvent = null;
+        private Address Address = null;
+
+        public OrgEventForm(OrgEvent orgEvent = null, Address address = null)
+        {
+            OrgEvent = orgEvent;
+            Address = address;
+            InitializeComponent();
+        }
+
+        private void button_ok_Click(object sender, EventArgs e)
+        {
+            var address = MongoRepositoryAddresses.Get(textBox_street.Text, textBox_house.Text, textBox_building.Text,
+                textBox_apartment.Text);
+
+            if (address == null)
+            {
+                MessageBox.Show("Адрес не занесен в базу!");
+                return;
+            }
+
+            var orgEvent = new OrgEvent
+            {
+                Id = OrgEvent?.Id ?? Guid.NewGuid(),
+                EventType = GetEventType(),
+                CounterType = GetCounterType(),
+                AddressId = address.Id,
+                Date = dateTimePicker_date.Value,
+                ImplementerId = ((ComboBoxItem)comboBox_implementer.SelectedItem).HiddenValue,
+                Place = string.IsNullOrEmpty(textBox_place.Text) ? null : textBox_place.Text
+            };
+
+            MongoRepositoryOrgEvent.Upsert(orgEvent);
+
+            Close();
+        }
+
+        private void button_cancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void EventForm_Load(object sender, EventArgs e)
+        {
+            var implementers = MongoRepositoryImplementers.GetAll();
+            foreach (var imp in implementers)
+            {
+                comboBox_implementer.Items.Add(new ComboBoxItem(imp.Name, imp.Id));
+            }
+            if (OrgEvent == null)
+            {
+                comboBox_implementer.SelectedIndex = 0;
+                comboBox_eventType.SelectedIndex = 0;
+                comboBox_counterType.SelectedIndex = 0;
+            }
+            else
+            {
+                textBox_street.Text = Address?.Street;
+                textBox_house.Text = Address?.House;
+                textBox_building.Text = Address?.Building;
+                textBox_apartment.Text = Address?.Apartment;
+                comboBox_eventType.SelectedIndex = (int)OrgEvent.EventType;
+                textBox_place.Text = OrgEvent.Place;
+                dateTimePicker_date.Value = OrgEvent.Date;
+                comboBox_counterType.SelectedIndex = (int) OrgEvent.CounterType;
+
+                var impId = OrgEvent.ImplementerId;
+                //var index = comboBox_implementer.Items.
+
+                //var impIndex = ((ComboBoxItem)comboBox_implementer.SelectedItem).HiddenValue,
+                //comboBox_implementer.SelectedIndex = comboBox_implementer
+            }
+
+        }
+
+        private EventType GetEventType()
+        {
+            var result = EventType.INSTALL;
+            var index = comboBox_eventType.SelectedIndex;
+            switch (index)
+            {
+                case 0:
+                    result = EventType.INSTALL;
+                    break;
+                case 1:
+                    result = EventType.REINSTALL;
+                    break;
+                case 2:
+                    result = EventType.VERIFICATION;
+                    break;
+            }
+            return result;
+        }
+
+        private CounterType GetCounterType()
+        {
+            var result = CounterType.COLD;
+            var index = comboBox_counterType.SelectedIndex;
+            switch (index)
+            {
+                case 0:
+                    result = CounterType.COLD;
+                    break;
+                case 1:
+                    result = CounterType.HOT;
+                    break;
+                case 2:
+                    result = CounterType.ELECTRO;
+                    break;
+            }
+            return result;
+        }
+    }
+
+    public class ComboBoxItem
+    {
+        string displayValue;
+        Guid hiddenValue;
+
+        //Constructor
+        public ComboBoxItem(string d, Guid h)
+        {
+            displayValue = d;
+            hiddenValue = h;
+        }
+
+        //Accessor
+        public Guid HiddenValue
+        {
+            get
+            {
+                return hiddenValue;
+            }
+        }
+
+        //Override ToString method
+        public override string ToString()
+        {
+            return displayValue;
+        }
+    }
+}
