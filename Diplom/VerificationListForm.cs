92787c5cdf4c48a588ca90afecb48ecbd011eb4b
skappa <denaltro@gmail.com>
2017-05-26 03:15:52 +0400

    added export document to excel

--- Diplom/VerificationListForm.cs
+++ Diplom/VerificationListForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 using Diplom.Models;
 using Microsoft.Office.Interop.Excel;
 
@@ -35,8 +36,16 @@ namespace Diplom
 
             var exist = File.Exists(textBox_path.Text);
             if (!exist) MessageBox.Show("Файл не найден!");
+
+            var verificationList = Parse();
+            Export(verificationList);
+
+        }
+
+        private List<Verification> Parse()
+        {
             var rowNumber = 0;
-            var verificationList = new List<Verification>();
+            var result = new List<Verification>();
             try
             {
                 Microsoft.Office.Interop.Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
@@ -50,16 +59,10 @@ namespace Diplom
                 for (int i = 1; i < xlRange.Rows.Count; i++)
                 {
                     rowNumber = i;
-
-                    //if (rowNumber == 234)
-                    //{
-                    //    var dd = 1;
-                    //}
-
                     var addressCell = (Range)xlWorksheet.Cells[rowNumber, 4];
                     if (addressCell.Font.Bold == true && !string.IsNullOrEmpty(addressCell.Value))
                     {
-                        if (verification != null) verificationList.Add(verification);
+                        if (verification != null) result.Add(verification);
 
                         verification = new Verification
                         {
@@ -88,6 +91,44 @@ namespace Diplom
                 MessageBox.Show(ex.StackTrace);
                 MessageBox.Show(ex.Message);
             }
+            return result;
+        }
+
+        private void Export(List<Verification> verificationList)
+        {
+            Microsoft.Office.Interop.Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
+            Workbook xlWorkBook = xlApp.Workbooks.Open(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Templates\\template_verification_list.xls"), 0, true, 5, "", "", true, XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+            _Worksheet xlWorksheet = (_Worksheet)xlWorkBook.Sheets[1];
+
+            int startRow = 5;
+
+            foreach (var verification in verificationList)
+            {
+                var start = startRow;
+                var end = startRow + verification.Counters.Count - 1;
+                var startCell = xlWorksheet.Cells[start, 1];
+                var endCell = xlWorksheet.Cells[end, 1];
+                var range = xlWorksheet.Range[startCell, endCell];
+                range.Merge(Type.Missing);
+                range.Style.HorizontalAlignment = XlHAlign.xlHAlignCenter;
+                range.Style.VerticalAlignment = XlVAlign.xlVAlignCenter;
+                range.Borders.LineStyle = XlLineStyle.xlContinuous;
+                range.BorderAround();
+
+                xlWorksheet.Cells[startRow, 1] = verification.Address;
+                foreach (var counter in verification.Counters)
+                {
+                    xlWorksheet.Cells[startRow, 2] = counter.StartCount;
+                    xlWorksheet.Cells[startRow, 3] = counter.EndCount;
+                    xlWorksheet.Cells[startRow, 4] = counter.StartDate;
+                    xlWorksheet.Cells[startRow, 5] = counter.EndDate;
+                    var rng = xlWorksheet.Range[xlWorksheet.Cells[startRow, 2], xlWorksheet.Cells[startRow, 5]];
+                    rng.Borders.LineStyle = XlLineStyle.xlContinuous;
+                    startRow++;
+                }
+            }
+
+            xlApp.Visible = true;
         }
 
     }
